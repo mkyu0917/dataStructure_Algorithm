a649d00c9689bc1927930eb72b3c6b193f54b6de
mkyu0917 <mkyu0917@gmail.com>
2021-12-13 13:29:54 +0900

    중앙값 찾기

--- src/findMedian.java
+++ src/findMedian.java
@@ -1,27 +1,40 @@
 import java.util.Scanner;
 
 public class findMedian {
+    static int findMedi(int a, int b, int c) {
+        // 1 4 2
+        if(a>=b)
+             if(b>c) //abc
+                 return b;
+             else if(a<=c) //cab
+                 return a;
+             else       //acb
+                 return c;
+       else if(b<=c) // c>b>a
+            return b;
+       else if(a<=c) //a<c
+            return c;
+       else
+            return a;
+
+
+    }
 
     public static void main(String[] args){
 
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
 
-        int median;
-        median = a;
-
-        if(c>median){
-            median =c;
-        }
-        if(median>b){
-            median = b;
-        }
+        System.out.println( "결과값: "+ findMedi(a,b,c));
 
 
-        System.out.println("중앙값은 " + median + " 입니다.");
 
     }
+
+
 }
+
+
